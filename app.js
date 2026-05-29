48ca1d674df90f13cb7bca0d91b82b46d353a6c9
cclarkesnhu <cameron.clarke@snhu.edu>
2026-05-29 19:02:58 -0400

    Module 3 completed baseline

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const hbs = require('hbs');
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
-var travelRouter = require('./app_server/routes/travel');  // Fixed: renamed to travelRouter
-var handlebars = require('hbs');
+var travelRouter = require('./app_server/routes/travel');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app_server', 'views'));
 
-// register handlebars partials
-handlebars.registerPartials(__dirname + './app_server/views/partials');  // Fixed: added missing underscore
+// register handlebars partials (https://www.npmjs.com/package/hbs)
+hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'));
 
 app.set('view engine', 'hbs');
 
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/travel', travelRouter);  // Fixed: changed 'travel' to '/travel' and travelRouter
+app.use('/travel', travelRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,32 +1,10 @@
-const tripsEndpoint = 'http://localhost:3000/api/trips';
-const options = {
-method: 'GET',
-headers: {
-'Accept': 'application/json'
+var fs = require('fs');
+
+var trips = JSON.parse(fs.readFileSync('./app_server/data/trips.json', 'utf8'));
+
+/*Get travel view*/
+const travel = (req, res) => {
+    res.render('travel', {title: 'Travlr Getaways', trips});
 }
-}
-//var fs = require('fs');
-//var trips =
-//JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
 
-/* GET travel view */
-const travel = async function(req , res, next) {
-    await fetch(tripsEndpoint, options)
-        .then(res => res.json())
-        .then(json => {
-            let message = null;
-            if(!(json instanceof Array)) {
-                message = json;
-                json = [];
-            }else{
-                if(!json.length) {
-                    message = "No trips exist in our database!";
-                }
-            }
-            res.render('travel', {title: 'Travlr Getaways', trips: json, message});
-        })
-
-    .catch(err => res.status(500).send(e.message));
-};
-
-module.exports = {travel};
+module.exports = { travel};
